df80eebe2192c8273d6f26e3384541eb95e9547a
aliyun2377909947 <>
2023-03-31 18:22:26 +0800

    feat: 以日期格式输出文件

--- index.js
+++ index.js
@@ -5,12 +5,24 @@ const path = require('path');
 let wordList = []
 const stringList = []
 const directoryPath = './pdf';
-const chunkSize = 8;
+const chunkSize = 10;
 
 // 读取文件夹下所有文件
 const files = fs.readdirSync(directoryPath);
 // 过滤出pdf文件
 const pdfFiles = files.filter((file) => path.extname(file).toLowerCase() === '.pdf');
+
+// 打乱数组顺序
+const shuffle = (arr) => arr.sort(() => Math.random() - 0.5);
+
+// 以 YYYY-MM-DD 格式返回当前日期
+const getToday = () => {
+  const date = new Date();
+  const year = date.getFullYear();
+  const month = date.getMonth() + 1;
+  const day = date.getDate();
+  return `${year}-${month}-${day}`;
+}
 
 const init = async () => {
   // 读取文件夹下所有pdf文件
@@ -24,15 +36,18 @@ const init = async () => {
     wordList = [...wordList, ...words]
   }
 
+  // 打乱顺序
+  shuffle(wordList)
+
   // 按照chunkSize分割数组
   for (let i = 0; i < wordList.length; i += chunkSize) {
     const chunk = wordList.slice(i, i + chunkSize);
-    const string = `（${chunk.join(',')}）`
+    const string = `（${chunk.join('，')}）`
     stringList.push(string);
   }
 
   // 写入文件
-  fs.writeFileSync('./result.txt', stringList.join('\n'));
+  fs.writeFileSync(`result/${getToday()}.txt`, stringList.join('\n'));
 }
 
 init()
